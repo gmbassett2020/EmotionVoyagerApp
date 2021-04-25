54cc87ea7d23e4f4ed03ade6852ae883adfed891
Gene_Bassett_sandy <sandyGit@curiosityQuest.xyz>
2021-04-25 15:08:51 -0700

    Initial App version.  Based off of basic app template.

--- MyFirstActivity/app/src/main/java/com/example/myfirstactivity/MainActivity.java
+++ MyFirstActivity/app/src/main/java/com/example/myfirstactivity/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.myfirstactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String EXTRA_MESSAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // respond to the update button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.focusWord);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
